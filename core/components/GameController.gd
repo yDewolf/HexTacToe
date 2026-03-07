308f40ffed93ff9caef70877d459e8dfa1432b60
yDewolf <andrersng@gmail.com>
2026-03-06 22:29:15 -0300

    Just a simple test

---
+++ core/components/GameController.gd
@@ -0,0 +1,67 @@
+extends Node
+
+@export var level_node: Node2D
+
+@export var background_layer: TileMapLayer
+@export var red_layer: TileMapLayer
+@export var blue_layer: TileMapLayer
+
+@export var current_turn_label: Label
+@export var reset_button: Button
+
+const TILESET_ID = 0
+
+enum Players {
+	RED,
+	BLUE
+}
+
+var turn: int = Players.RED
+
+func _ready() -> void:
+	self.on_reset()
+	reset_button.pressed.connect(on_reset)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Place"):
+		var mouse_coords = background_layer.get_local_mouse_position()
+		var local_coords = background_layer.local_to_map(mouse_coords)
+		if can_place_at(local_coords):
+			place_at(local_coords)
+			next_turn()
+
+func on_reset():
+	self.red_layer.clear()
+	self.blue_layer.clear()
+	self.turn = Players.RED
+
+
+func place_at(coords: Vector2):
+	#if not can_place_at(coords):
+		#return
+	
+	if self.turn == Players.RED:
+		self.red_layer.set_cell(coords, TILESET_ID, Vector2i(1, 1))
+		return
+	
+	self.blue_layer.set_cell(coords, TILESET_ID, Vector2i(0, 1))
+	return
+
+func can_place_at(coords: Vector2i) -> bool:
+	if self.background_layer.get_cell_atlas_coords(coords) == Vector2i(-1, -1):
+		return false
+	
+	if self.blue_layer.get_cell_atlas_coords(coords) != Vector2i(-1, -1):
+		return false
+	
+	if self.red_layer.get_cell_atlas_coords(coords) != Vector2i(-1, -1):
+		return false
+	
+	return true
+
+func next_turn():
+	self.turn = self.turn + 1
+	if self.turn >= Players.size():
+		self.turn = 0
+	
+	self.current_turn_label.text = str(Players.find_key(self.turn)) + "'s turn"
